2d81f894aa14760d4986e9c3ea3fa009e2b36fdb
tylertw2@illinois.edu <Zx09mn12>
2018-12-04 19:51:17 -0600

    Added Hz labels.

---
+++ app/src/main/java/com/example/trtnw/equalizerv5/CustomPreset.java
@@ -0,0 +1,14 @@
+package com.example.trtnw.equalizerv5;
+
+public class CustomPreset {
+    private String name;
+    private int[] levels;
+    public CustomPreset(String setName, int[] setLevels) {
+        this.name = setName;
+        this.levels = setLevels;
+    }
+    //onEvent create new
+    public void setLevel(int bandIndex, int level) {
+
+    }
+}
--- app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
+++ app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
@@ -1,13 +1,79 @@
 package com.example.trtnw.equalizerv5;
 
+import java.util.ArrayList;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+
 import android.support.v7.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.media.audiofx.Equalizer;
+
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Spinner;
+
+import com.h6ah4i.android.widget.verticalseekbar.VerticalSeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer myMediaPlayer;
+    private Equalizer myEqualizer;
 
+    /**
+     * Executed when app is loaded.
+     * @param savedInstanceState Data supplied to onCreate if activity is restarted or reoriented
+     */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //show_spinner_presets();
+
+
+    }
+    /**
+     * Shows spinner with presets to choose from
+     */
+    public void show_spinner_presets() {
+        //spinner1 setup
+        ArrayList<String> presetNames = new ArrayList<>();
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, presetNames);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner dropdown1 = findViewById(R.id.spinner1);
+        //get list of presets in mEqualizer class
+        //short is required by getPresetName(i)
+        for (short i = 0; i < myEqualizer.getNumberOfPresets(); i++) {
+            presetNames.add(myEqualizer.getPresetName(i));
+        }
+        dropdown1.setAdapter(spinnerAdapter);
+        //set preset selected!!!!!!!
+        dropdown1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //first preset listed is set as default current preset
+                myEqualizer.usePreset((short) position);
+                //get number of bands from equalizer engine
+                short numberFrequencyBands = myEqualizer.getNumberOfBands();
+                //get lowest setting for each band
+                short bottomBandLevel = myEqualizer.getBandLevelRange()[0];
+                //set seekBar indicator based on each band
+                for (short i = 0; i < numberFrequencyBands; i++) {
+                    VerticalSeekBar seekBar = findViewById(i);
+                    //indicate current gain or loss in level
+                    seekBar.setProgress(myEqualizer.getBandLevel((short) (i - bottomBandLevel)));
+                    //https://stackoverflow.com/questions/35831900/equalizer-getbandleveli-returns-value-0
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
+
+
