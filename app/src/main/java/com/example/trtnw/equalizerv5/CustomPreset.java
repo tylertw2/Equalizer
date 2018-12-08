d6910901a5500b2a0b4e82cb53b94b699c621694
tylertw2@illinois.edu <Zx09mn12>
2018-12-08 01:41:21 -0600

    Mostly working.

--- app/src/main/java/com/example/trtnw/equalizerv5/CustomPreset.java
+++ app/src/main/java/com/example/trtnw/equalizerv5/CustomPreset.java
@@ -12,24 +12,28 @@ public class CustomPreset {
     }
     //Hard-code the presets
     //private CustomPreset[] myPresets = new CustomPreset[10];
-    private static CustomPreset preset0 = new CustomPreset("Default", new short[]{0, 0, 0, 0, 0});
-    private static CustomPreset preset1 = new CustomPreset("Bass+High Boost", new short[]{100, 0, -80, -50, 20});
-    private static CustomPreset preset2 = new CustomPreset("Bass Boost", new short[]{100, 10, -30, -30, -30});
-    private static CustomPreset preset3 = new CustomPreset("High Boost", new short[]{-30, -30, -30, 10, 50});
-    private static CustomPreset preset4 = new CustomPreset("Classical", new short[]{10, 30, -70, -10, 25});
-    private static CustomPreset preset5 = new CustomPreset("Dance", new short[]{50, 10, -30, -55, -23});
-    private static CustomPreset preset6 = new CustomPreset("Rock", new short[]{20, -50, -28, 40, 25});
-    private static CustomPreset preset7 = new CustomPreset("Perfect", new short[]{60, 60, 10, 70, 70});
+    private static CustomPreset preset0 = new CustomPreset("Default", new short[]{100, 200, 300, 400, 500});
+    private static CustomPreset preset1 = new CustomPreset("Bass+High Boost", new short[]{100, 0, -800, -500, 200});
+    private static CustomPreset preset2 = new CustomPreset("Bass Boost", new short[]{100, 100, -0, -300, -300});
+    private static CustomPreset preset3 = new CustomPreset("High Boost", new short[]{-300, -300, -300, 100, 500});
+    private static CustomPreset preset4 = new CustomPreset("Classical", new short[]{100, 300, -700, -100, 300});
+    private static CustomPreset preset5 = new CustomPreset("Dance", new short[]{500, 100, -300, -600, -200});
+    private static CustomPreset preset6 = new CustomPreset("Rock", new short[]{200, -500, -300, 400, 200});
+    private static CustomPreset preset7 = new CustomPreset("Perfect", new short[]{200, -200, -200, -100, -100});
     //private CustomPreset preset8 = new CustomPreset("Bass/High Boost", new short[]{100, 0, -80, -50, 20});
     //myPresets[0] = bhBoost;
     //myPresets[1] = new CustomPreset("Bass Boost", new short {});
 
     public static ArrayList<CustomPreset> getPresetList() {
         ArrayList<CustomPreset> presetList = new ArrayList<>();
-        for(int i = 0; i < 8; i++) {
-            CustomPreset a = (CustomPreset) (Object) ("preset" + Integer.toString(i));
-            presetList.add(a);
-        }
+        presetList.add(preset0);
+        presetList.add(preset1);
+        presetList.add(preset2);
+        presetList.add(preset3);
+        presetList.add(preset4);
+        presetList.add(preset5);
+        presetList.add(preset6);
+        presetList.add(preset7);
         return presetList;
     }
 
@@ -40,10 +44,26 @@ public class CustomPreset {
     public String getName(short index) {
         return getPresetList().get(index).name;
     }
-    public short[] getLevels(short index) {
-        return getPresetList().get(index).levels;
+    public ArrayList<Short> getLevels(short index) {
+        ArrayList<Short> toReturn = new ArrayList<>();
+        System.out.println();
+        for (int i = 0; i < 5; i++) {
+            System.out.print("penis:   " + getPresetList().get(index).levels[index]);
+            toReturn.add(getPresetList().get(index).levels[index]);
+        }
+        return toReturn;
+    }
+    public static ArrayList<Short> getLevelsFromCP(CustomPreset cp) {
+        ArrayList<Short> toReturn = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            toReturn.add(cp.levels[i]);
+        }
+        return toReturn;
     }
     /**
+    public static ArrayList<ArrayList<Short>> getAllLevels() {
+
+    }
     public short[] getLevels(CustomPreset preset) {
         return preset.levels;
     }
--- app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
+++ app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.trtnw.equalizerv5;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import android.widget.ArrayAdapter;
 import android.widget.AdapterView;
+import android.widget.LinearLayout;
 
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,6 +14,7 @@ import android.media.audiofx.Equalizer;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -23,6 +26,7 @@ import com.h6ah4i.android.widget.verticalseekbar.VerticalSeekBar;
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer myMediaPlayer;
     private Equalizer myEqualizer;
+    private LinearLayout myLinearLayout;
 
     public String[] names = new String[11];
     public short[][] presetLevels = new short[11][0];
@@ -36,22 +40,45 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setVolumeControlStream(AudioManager.STREAM_MUSIC);
         //Initiate switch
         Switch simpleSwitch = findViewById(R.id.switch1);
         simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
         simpleSwitch.setTextOff("Off");
         //show_spinner_presets();
         //Create equalizer with default settings of 0dB
-        myEqualizer = new Equalizer(0, getTaskId());
+        // Create the MediaPlayer
+//        you need to put your audio file in the res/raw folder
+//        - the filename must be test_audio_file or
+//        change it below to match your filename
+        myMediaPlayer = MediaPlayer.create(this, R.raw.breathe);
+        myMediaPlayer.start();
+        //myEqualizer = new Equalizer(0, getTaskId());
+        myEqualizer = new Equalizer(0, myMediaPlayer.getAudioSessionId());
         myEqualizer.setEnabled(true);
+        /** PRINT OUT EQUALIZER PRESETS */
+        String[] music_styles = new String[myEqualizer.getNumberOfPresets()];
+        for(int k=0; k <myEqualizer.getNumberOfPresets() ; k++) {
+            music_styles[k] = myEqualizer.getPresetName((short) k);
+            myEqualizer.usePreset((short) k);
+            for (short j = 0; j < 5; j++) {
+                System.out.println("band " + j + ": " + myEqualizer.getBandLevel(j));
+            }
+            System.out.println(k + "  " + music_styles[k]);
+        }
         //loadPresets();
-
+        //TEST SHIT DELETE LATER
+        //myMediaPlayer = MediaPlayer.create(this, R.raw.test_audio_file);
+        //myMediaPlayer.start();
+        setUI();
     }
     @Override
     protected void onPause() {
         super.onPause();
-        if (isFinishing() && myMediaPlayer != null) {
+        if (myMediaPlayer != null) {
             myEqualizer.release();
+            myMediaPlayer.release();
+            myMediaPlayer = null;
         }
     }
 
@@ -63,28 +90,30 @@ public class MainActivity extends AppCompatActivity {
         // REPLACE FOR LOOP WITH CustomPreset.java STUFF!!!!!!!!
         //load CustomPreset class
         ArrayList<CustomPreset> presetList = CustomPreset.getPresetList();
+        for (int i = 0; i < 8; i++) {
+            System.out.println(i + "    " + CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)));
+        }
         ArrayList<String> presetNames = new ArrayList<>();
-        for (short i = 0; i <= 7; i++) {
-            presetNames.add(presetList.get(i).getName(i));
-        }
-        ArrayList<ArrayList<Short>> allPresetLevels = new ArrayList<>();
-        for (short i = 0; i <= 7; i++) {
-            ArrayList<Short> presetLevels = new ArrayList<>();
-            for (short j = i; j <= 7; j++) {
-                presetLevels.add(presetList.get((int) i).getLevels(i)[(int) i]);
-            }
-            allPresetLevels.add(presetLevels);
-        }
-        //final ArrayList<Short> presetLevelsInnerClass = presetLevels;
-        final ArrayList<ArrayList<Short>> allPresetLevelsInnerClass = allPresetLevels;
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, presetNames);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Spinner dropdown1 = findViewById(R.id.spinner1);
+        Spinner presetSpinner = findViewById(R.id.spinner1);
+        // get list of all preset's levels as ArrayList of ArrayLists
+        ArrayList<ArrayList<Short>> presetLevels = new ArrayList<>();;
+        //get list of presetNames
+        for (short i = 0; i < 8; i++) {
+            presetLevels.add(CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)));
+            //presetLevels.set(i, Arrays.asList(CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)))))
+            presetNames.add(presetList.get(i).getName(i));
+            //presetLevels.set(i, presetList.get(i).getLevels(i));
+        }
+        //final ArrayList<Short> presetLevelsInnerClass = presetLevels;
+        final ArrayList<ArrayList<Short>> allPresetLevelsInnerClass = presetLevels;
+
         //get list of presets in mEqualizer class
         //short is required by getPresetName(i)
-        dropdown1.setAdapter(spinnerAdapter);
+        presetSpinner.setAdapter(spinnerAdapter);
         //set preset selected!!!!!!!
-        dropdown1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        presetSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //first preset listed is set as default current preset
@@ -92,12 +121,34 @@ public class MainActivity extends AppCompatActivity {
                 //get lowest setting for each band
                 useSettings(allPresetLevelsInnerClass.get(position));
                 short bottomBandLevel = myEqualizer.getBandLevelRange()[0];
-                for (int i = 0; i <= 5; i++) {
-                    VerticalSeekBar bar = (VerticalSeekBar) (Object) ("seekBar" + Integer.toString(i));
-                    bar = findViewById(i);
-                    bar.setProgress(myEqualizer.getBandLevel((short) (i - bottomBandLevel)));
+                VerticalSeekBar bar1 = findViewById(R.id.seekBar1);
+                bar1.setProgress(allPresetLevelsInnerClass.get(position).get(0));
+                TextView tv1 = findViewById(R.id.textView1);
+                tv1.setText(allPresetLevelsInnerClass.get(position).get(0) + "dB");
+                VerticalSeekBar bar2 = findViewById(R.id.seekBar2);
+                bar2.setProgress(allPresetLevelsInnerClass.get(position).get(1));
+                TextView tv2 = findViewById(R.id.textView2);
+                tv2.setText(allPresetLevelsInnerClass.get(position).get(1) + "dB");
+                VerticalSeekBar bar3 = findViewById(R.id.seekBar3);
+                bar3.setProgress(allPresetLevelsInnerClass.get(position).get(2));
+                TextView tv3 = findViewById(R.id.textView3);
+                tv3.setText(allPresetLevelsInnerClass.get(position).get(2) + "dB");
+                VerticalSeekBar bar4 = findViewById(R.id.seekBar4);
+                bar4.setProgress(allPresetLevelsInnerClass.get(position).get(3));
+                TextView tv4 = findViewById(R.id.textView4);
+                tv4.setText(allPresetLevelsInnerClass.get(position).get(3) + "dB");
+                VerticalSeekBar bar5 = findViewById(R.id.seekBar5);
+                bar5.setProgress(allPresetLevelsInnerClass.get(position).get(4));
+                TextView tv5 = findViewById(R.id.textView5);
+                tv5.setText(allPresetLevelsInnerClass.get(position).get(4) + "dB");
+                /**
+                for (int i = 0; i < 5; i++) {
+                    //VerticalSeekBar bar = (VerticalSeekBar) (Object) ("seekBar" + Integer.toString(i));
+                    VerticalSeekBar bar = findViewById(R.id.seekBar1);
+                    bar.setProgress(allPresetLevelsInnerClass.get(position).get(i));
+                    //bar.setProgress(myEqualizer.getBandLevel((short) (i - bottomBandLevel)));
                         // //https://stackoverflow.com/questions/35831900/equalizer-getbandleveli-returns-value-0
-                }
+                } */
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
@@ -105,9 +156,54 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void setUI() {
+        for (short i = 0; i < 5; i++) {
+            //            set up linear layout to contain each seekBar
+            LinearLayout seekBarRowLayout = new LinearLayout(this);
+            seekBarRowLayout.setOrientation(LinearLayout.HORIZONTAL);
+            //            **********  the seekBar  **************
+//            set the layout parameters for the seekbar
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT);
+            layoutParams.weight = 1;
+
+//            create a new seekBar
+            VerticalSeekBar seekBar = new VerticalSeekBar(this);
+//            give the seekBar an ID
+            seekBar.setId(i);
+
+            seekBar.setLayoutParams(layoutParams);
+            seekBar.setMax(30);
+//            set the progress for this seekBar
+            seekBar.setProgress(myEqualizer.getBandLevel(i));
+
+//            change progress as its changed by moving the sliders
+            final short thisI = i;
+            seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                    myEqualizer.setBandLevel(thisI, (short) (progress - 15));
+                }
+
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                    //not used
+                }
+
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                    //not used
+                }
+            });
+           // myLinearLayout.addView(seekBarRowLayout);
+
+            //        show the spinner
+            show_spinner_presets();
+        }
+    }
+
     public void useSettings(ArrayList<Short> levels) {
-        for (short i = 0; i <=5; i++) {
+        for (short i = 0; i < 5; i++) {
             myEqualizer.setBandLevel(i, levels.get((int) i));
+            System.out.println("band " + i + ":  " + myEqualizer.getBandLevel(i));
         }
     }
 
