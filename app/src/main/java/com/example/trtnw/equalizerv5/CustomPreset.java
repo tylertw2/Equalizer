071f3a287c762d70a0fe20affa1d5c4e9e7fbf08
tylertw2@illinois.edu <Zx09mn12>
2018-12-09 21:49:08 -0600

    Commented out stuff

--- app/src/main/java/com/example/trtnw/equalizerv5/CustomPreset.java
+++ app/src/main/java/com/example/trtnw/equalizerv5/CustomPreset.java
@@ -3,7 +3,7 @@ package com.example.trtnw.equalizerv5;
 import java.util.ArrayList;
 
 public class CustomPreset {
-    private String name;
+    private String name ;
     private short[] levels;
 
     public CustomPreset(String setName, short[] setLevels) {
@@ -18,7 +18,7 @@ public class CustomPreset {
     private static CustomPreset preset3 = new CustomPreset("Dad Rock", new short[]{200, -500, -300, 400, 200});
     private static CustomPreset preset4 = new CustomPreset("Bourgeois", new short[]{100, 300, -700, -100, 300}); //classical
     private static CustomPreset preset5 = new CustomPreset("Never Enough Bass", new short[]{1500, 1000, -0, -800, -1000});
-    private static CustomPreset preset6 = new CustomPreset("Global Warming is a Hoax", new short[]{-1500, -1500, -1500, 1500, 1500}); //it's "bassless"... heh
+    private static CustomPreset preset6 = new CustomPreset("Global Warming is a Hoax", new short[]{-1500, -1500, 0, 1500, 1500}); //it's "bassless"... heh
     private static CustomPreset preset7 = new CustomPreset("Very Quiet", new short[]{1500, 1500, 1500, 1500, 1500}); //i'm not sorry
     //private CustomPreset preset8 = new CustomPreset("Bass/High Boost", new short[]{100, 0, -80, -50, 20});
     //myPresets[0] = bhBoost;
@@ -51,12 +51,12 @@ public class CustomPreset {
         return toReturn;
     }
     /**
-    public static ArrayList<ArrayList<Short>> getAllLevels() {
+     public static ArrayList<ArrayList<Short>> getAllLevels() {
 
-    }
-    public short[] getLevels(CustomPreset preset) {
-        return preset.levels;
-    }
+     }
+     public short[] getLevels(CustomPreset preset) {
+     return preset.levels;
+     }
      */
 
 }
--- app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
+++ app/src/main/java/com/example/trtnw/equalizerv5/MainActivity.java
@@ -28,37 +28,41 @@ public class MainActivity extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Allow app to control volume of device
         setVolumeControlStream(AudioManager.STREAM_MUSIC);
         //Create MediaPlayer with default settings of 0dB for all bands
         //Play mp3 file from res/raw/
+        //To use with other music, disable next 2 lines, and use android musicplayer to play mp3s saved to the device
         myMediaPlayer = MediaPlayer.create(this, R.raw.vulf);
         myMediaPlayer.start();
         //Enable equalizer object
         myEqualizer = new Equalizer(0, myMediaPlayer.getAudioSessionId());
         myEqualizer.setEnabled(true);
-        /** PRINT OUT EQUALIZER PRESETS */
-        String[] music_styles = new String[myEqualizer.getNumberOfPresets()];
-        for(int k=0; k <myEqualizer.getNumberOfPresets() ; k++) {
-            music_styles[k] = myEqualizer.getPresetName((short) k);
-            myEqualizer.usePreset((short) k);
-            for (short j = 0; j < 5; j++) {
-                System.out.println("freq: " + myEqualizer.getBandFreqRange(j).toString());
-                System.out.println("band " + j + ": " + myEqualizer.getBandLevel(j));
-            }
-            System.out.println(k + "  " + music_styles[k]);
-        }
+        //Setup spinner UI
         show_spinner_presets();
+        /** Print out built-in equalizer settings (only used as guidelines to make our own presets)
+         String[] music_styles = new String[myEqualizer.getNumberOfPresets()];
+         for(int k=0; k <myEqualizer.getNumberOfPresets() ; k++) {
+         music_styles[k] = myEqualizer.getPresetName((short) k);
+         myEqualizer.usePreset((short) k);
+         for (short j = 0; j < 5; j++) {
+         System.out.println("freq: " + myEqualizer.getBandFreqRange(j).toString());
+         System.out.println("band " + j + ": " + myEqualizer.getBandLevel(j));
+         }
+         System.out.println(k + "  " + music_styles[k]);
+         }
+         */
     }
     @Override
     protected void onPause() {
         super.onPause();
         //basically do nothing
-        /** testing
-        if (myMediaPlayer != null) {
-            myEqualizer.release();
-            myMediaPlayer.release();
-            myMediaPlayer = null;
-        } */
+        /** Disable to pause app when tabbed out
+         if (myMediaPlayer != null) {
+         myEqualizer.release();
+         myMediaPlayer.release();
+         myMediaPlayer = null;
+         } */
     }
 
     /**
@@ -66,55 +70,56 @@ public class MainActivity extends AppCompatActivity {
      */
     public void show_spinner_presets() {
         ArrayList<CustomPreset> presetList = CustomPreset.getPresetList();
-        /** Print out band levels from all presets */
-        for (int i = 0; i < CustomPreset.getNumPresets(); i++) {
-            System.out.println(i + "    " + CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)));
-        }
+        /** Print out band levels from all presets
+         for (int i = 0; i < CustomPreset.getNumPresets(); i++) {
+         System.out.println(i + "    " + CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)));
+         } */
         ArrayList<String> presetNames = new ArrayList<>();
         ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, presetNames);
         spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner presetSpinner = findViewById(R.id.spinner1);
         // get list of all preset's levels as ArrayList of ArrayLists
         ArrayList<ArrayList<Short>> presetLevels = new ArrayList<>();;
-        //get list of presetNames
+        //get list of presetNames and load it into the spinner
+        //get list of presetsLevels for onItemSelected
         for (short i = 0; i < CustomPreset.getNumPresets(); i++) {
             presetLevels.add(CustomPreset.getLevelsFromCP(CustomPreset.getPresetList().get(i)));
             presetNames.add(presetList.get(i).getName(i));
         }
-        final ArrayList<ArrayList<Short>> allPresetLevelsInnerClass = presetLevels;
-        //get list of presets in myEqualizer class
+        final ArrayList<ArrayList<Short>> allPresetLevels = presetLevels;
+        //set adapter for the spinner
         presetSpinner.setAdapter(spinnerAdapter);
-        //use preset selected
+        //When a preset is selected from the spinner, use it's settings and change UI elements
         presetSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 //first preset listed is set as default current preset
-                useSettings(allPresetLevelsInnerClass.get(position));
+                useSettings(allPresetLevels.get(position));
                 //set progress for each seekBar
                 VerticalSeekBar bar1 = findViewById(R.id.seekBar1);
-                bar1.setProgress((int)((double) allPresetLevelsInnerClass.get(position).get(0)*0.0333 + 50));
+                bar1.setProgress((int)((double) allPresetLevels.get(position).get(0)*0.0333 + 50));
                 TextView tv1 = findViewById(R.id.textView1);
-                tv1.setText((allPresetLevelsInnerClass.get(position).get(0)/100) + "dB");
+                tv1.setText((allPresetLevels.get(position).get(0)/100) + "dB");
                 VerticalSeekBar bar2 = findViewById(R.id.seekBar2);
-                bar2.setProgress((int)((double) allPresetLevelsInnerClass.get(position).get(1)*0.0333 + 50));
+                bar2.setProgress((int)((double) allPresetLevels.get(position).get(1)*0.0333 + 50));
                 TextView tv2 = findViewById(R.id.textView2);
-                tv2.setText(allPresetLevelsInnerClass.get(position).get(1)/100 + "dB");
+                tv2.setText(allPresetLevels.get(position).get(1)/100 + "dB");
                 VerticalSeekBar bar3 = findViewById(R.id.seekBar3);
-                bar3.setProgress((int)((double) allPresetLevelsInnerClass.get(position).get(2)*0.0333 + 50));
+                bar3.setProgress((int)((double) allPresetLevels.get(position).get(2)*0.0333 + 50));
                 TextView tv3 = findViewById(R.id.textView3);
-                tv3.setText(allPresetLevelsInnerClass.get(position).get(2)/100 + "dB");
+                tv3.setText(allPresetLevels.get(position).get(2)/100 + "dB");
                 VerticalSeekBar bar4 = findViewById(R.id.seekBar4);
-                bar4.setProgress((int)((double) allPresetLevelsInnerClass.get(position).get(3)*0.0333 + 50));
+                bar4.setProgress((int)((double) allPresetLevels.get(position).get(3)*0.0333 + 50));
                 TextView tv4 = findViewById(R.id.textView4);
-                tv4.setText(allPresetLevelsInnerClass.get(position).get(3)/100 + "dB");
+                tv4.setText(allPresetLevels.get(position).get(3)/100 + "dB");
                 VerticalSeekBar bar5 = findViewById(R.id.seekBar5);
-                bar5.setProgress((int)((double) allPresetLevelsInnerClass.get(position).get(4)*0.0333 + 50));
+                bar5.setProgress((int)((double) allPresetLevels.get(position).get(4)*0.0333 + 50));
                 TextView tv5 = findViewById(R.id.textView5);
-                tv5.setText(allPresetLevelsInnerClass.get(position).get(4)/100 + "dB");
+                tv5.setText(allPresetLevels.get(position).get(4)/100 + "dB");
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-
+                //Not used
             }
         });
     }
@@ -123,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         //Most android phones only have 5 frequency bands: 60, 230, 910, 3600 and 14000Hz
         for (short i = 0; i < 5; i++) {
             myEqualizer.setBandLevel(i, levels.get((int) i));
-            System.out.println("band " + i + ":  " + myEqualizer.getBandLevel(i));
+            //System.out.println("band " + i + ":  " + myEqualizer.getBandLevel(i));
         }
     }
 }
